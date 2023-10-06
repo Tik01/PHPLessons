396ad1a652373b491a022870d4207485340be177
Tik01 <sarukhanyantik@gmail.com>
2023-10-06 17:06:41 +0400

    Add Create Method

--- my_app/app/Http/Controllers/PostController.php
+++ my_app/app/Http/Controllers/PostController.php
@@ -2,11 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index(){
-        return 'aaaaaaaaaa';
+    public function AddPerson(){
+        $mens = [[
+            'name' => "AAAAA",
+            'lastname'=>'AAAAA',
+            'age'=>21,
+        ],
+        [
+            'name' => "BBBBB",
+            'lastname'=>'BBBBB',
+            'age'=>21,  
+        ],
+        [
+            'name' => "CCCCC",
+            'lastname'=>'CCCCC',
+            'age'=>21,
+        ]];
+        foreach ($mens as $men) {
+        Post::create($men);           
+        }
+        dd('CREATE METHOD');
     }
 }
--- my_app/routes/web.php
+++ my_app/routes/web.php
@@ -13,4 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', action:'PostController@index');
+Route::get('/', action:'PostController@AddPerson');
+// Route::get('/', action:'PostController@index');
+
